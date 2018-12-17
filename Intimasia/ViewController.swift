662ffb3017c0a72bba54a32e6e7ba854bec5a75f
Mohit Vensiyani <mohit30vensiyani@gmail.com>
2018-12-17 14:14:36 +0530

    Initial Commit

---
+++ Intimasia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intimasia
+//
+//  Created by Mohit on 17/12/18.
+//  Copyright © 2018 Mohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
